9e550904b2439680f1f3df0bbdb752b524de8b84
diewland <diewland@gmail.com>
2023-03-29 09:06:14 +0700

    clean up

--- ethdenver23/submit.js
+++ ethdenver23/submit.js
@@ -1,3 +1,12 @@
+require('dotenv').config()
+const { PRIVATE_KEY } = process.env
+const { ethers, keccak256, toUtf8Bytes, recoverAddress, formatUnits } = require("ethers")
+
+// contract config
+const RPC_BASE_GOERLI = 'https://goerli.base.org'
+const RIDDLE_ADDR = '0xc1e40f9FD2bc36150e2711e92138381982988791'
+const RIDDLE_ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"close","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"finishingTimes","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getLeaderboardStats","outputs":[{"internalType":"address[]","name":"","type":"address[]"},{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"hasSolvedChallenge1","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"hasSolvedChallenge2","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"isOnLeaderboard","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isOpenFlag","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"leaderboard","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"open","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"previousSignature","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"riddleAnswer","type":"string"}],"name":"solveChallenge1","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"riddleAnswer","type":"string"},{"internalType":"bytes","name":"signature","type":"bytes"}],"name":"solveChallenge2","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"riddleAnswer","type":"string"},{"internalType":"address","name":"signer","type":"address"},{"internalType":"bytes","name":"signature","type":"bytes"}],"name":"solveChallenge3","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"solvedChallenge1","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"solvedChallenge2","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userWhoUsedSigner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
+
 // https://www.coinbase.com/bounty/ethdenver23
 // https://goerli.basescan.org/address/0xc1e40f9FD2bc36150e2711e92138381982988791#code
 const RIDDLE_1_HASH = '0x3896ee3a8be6143be3fa1938adbae827fc724b5ff649501e7fd8c0c5352cbafa'
@@ -6,19 +15,18 @@ const RIDDLE_3_HASH = '0x3cd65f6089844a3c6409b0acc491ca0071a5672c2ab2a071f197011
 /// @dev calculated as ECDSA.toEthSignedMessageHash(RIDDLE_3_HASH)
 const RIDDLE_3_ETH_MESSAGE_HASH = '0x20a1626365cea00953c957fd02ddc4963990d404232d4e58acb66f46c59d9887'
 
-// contract config
-const dotenv = require('dotenv')
-dotenv.config()
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const { ethers, keccak256, toUtf8Bytes, formatUnits } = require("ethers")
-const RPC_BASE_GOERLI = 'https://goerli.base.org'
-const RIDDLE_ADDR = '0xc1e40f9FD2bc36150e2711e92138381982988791'
-const RIDDLE_ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"close","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"finishingTimes","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getLeaderboardStats","outputs":[{"internalType":"address[]","name":"","type":"address[]"},{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"hasSolvedChallenge1","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"hasSolvedChallenge2","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"isOnLeaderboard","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isOpenFlag","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"leaderboard","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"open","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"previousSignature","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"riddleAnswer","type":"string"}],"name":"solveChallenge1","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"riddleAnswer","type":"string"},{"internalType":"bytes","name":"signature","type":"bytes"}],"name":"solveChallenge2","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"riddleAnswer","type":"string"},{"internalType":"address","name":"signer","type":"address"},{"internalType":"bytes","name":"signature","type":"bytes"}],"name":"solveChallenge3","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"solvedChallenge1","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"solvedChallenge2","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userWhoUsedSigner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
-
 // prepare contract
 let provider = new ethers.JsonRpcProvider(RPC_BASE_GOERLI)
 let wallet = new ethers.Wallet(PRIVATE_KEY, provider)
 let contract = new ethers.Contract(RIDDLE_ADDR, RIDDLE_ABI, wallet)
+
+// submit template
+let submit = async (no, callback) => {
+  let out = null
+  try { out = await callback() }
+  catch (err) { out = err.reason }
+  console.log(`submit #${no}:`, out)
+}
 
 async function main() {
 
@@ -44,37 +52,21 @@ async function main() {
   console.log('---')
 
   // RIDDLE#1
-  let out1 = null
-  try {
-    out1 =  await contract.solveChallenge1.send(ANSWER_1)
-  }
-  catch(err) {
-    out1 = err.reason
-  }
-  console.log('submit #1:', out1)
+  await submit(1, async _ => contract.solveChallenge1.send(ANSWER_1))
 
   // RIDDLE#2
-  let out2 = null
-  try {
+  await submit(2, async _ => {
     let signature = await wallet.signMessage(RIDDLE_2_HASH)
-    out2 =  await contract.solveChallenge2.send(ANSWER_2, signature)
-  }
-  catch(err) {
-    out2 = err.reason
-  }
-  console.log('submit #2:', out2)
+    return contract.solveChallenge2.send(ANSWER_2, signature)
+  })
 
   // RIDDLE#3
   let out3 = null
-  try {
+  await submit(3, async _ => {
     let signature = await wallet.signMessage(RIDDLE_3_HASH)
-    let signer = wallet.address // TODO ???
-    out3 =  await contract.solveChallenge3.send(ANSWER_3, signer, signature)
-  }
-  catch(err) {
-    out3 = err.reason
-  }
-  console.log('submit #3:', out3)
+    let signer = recoverAddress(RIDDLE_3_ETH_MESSAGE_HASH, signature)
+    return contract.solveChallenge3.send(ANSWER_3, signer, signature)
+  })
 
 }
 main().then(_ => process.exit(0)).catch(error => { console.error(error); process.exit(1) })
